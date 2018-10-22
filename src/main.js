051506d1fefea002c94457c3644854649c955d47
Herikz Nawarro Mendes Rodrigues <herikz.rodrigues@iema.interno>
2018-10-22 12:27:12 -0200

    Add quasar plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
+import './styles/quasar.styl'
+import 'quasar-framework/dist/quasar.ie.polyfills'
+import lang from 'quasar-framework/i18n/pt-br'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}, i18n: lang
+})
+
 Vue.config.productionTip = false
 
 new Vue({
